3cd0111b4c49e6d7f7755d121754bdd557944710
Manitra Andriamitondra <m.andriamitondra@criteo.com>
2016-01-01 00:12:06 +0700

    Adding test for ParamReader

--- src/DependencyReader.CLI.Tests/LoggerTests.cs
+++ src/DependencyReader.CLI.Tests/LoggerTests.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DependencyReader.CLI;
+﻿using System.IO;
 using NUnit.Framework;
 namespace DependencyReader.CLI
 {
---
+++ src/DependencyReader.CLI.Tests/ParamReaderTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+namespace DependencyReader.CLI
+{
+    [TestFixture]
+    public class ParamReaderTests
+    {
+        [Test]
+        public void Read_WithData_ReturnsSame()
+        {
+            var target = new ParamReader();
+
+            const string input = "a-random-path";
+            var result = target.Read(new[] { input });
+
+            Assert.AreEqual(input, result.TargetPath);
+        }
+
+        [Test]
+        public void Read_WithoutData_ReturnsCurrentFolder()
+        {
+            var target = new ParamReader();
+
+            var result = target.Read(new string[0]);
+
+            Assert.AreEqual(".", result.TargetPath);
+        }
+
+    }
+}
