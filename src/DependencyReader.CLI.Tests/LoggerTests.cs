59ba5285b82c0bb5305a4734d2759151db08bb1f
Manitra Andriamitondra <m.andriamitondra@criteo.com>
2015-12-31 23:58:22 +0700

    Creating the test project and testing the Logger class

---
+++ src/DependencyReader.CLI.Tests/LoggerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DependencyReader.CLI;
+using NUnit.Framework;
+namespace DependencyReader.CLI
+{
+    [TestFixture]
+    public class LoggerTests
+    {
+        [Test]
+        public void Log_ContainsAllFields()
+        {
+            using(var writer = new StringWriter())
+            {
+                var entity = new DependencyInfo
+                {
+                    Parent = new AssemblyInfo{ Name="parent1", Version="1.0" },
+                    Child = new AssemblyInfo{ Name="child1", Version = "2.0"},
+                };
+
+                var target = new Logger(writer);
+                target.Log(entity);
+                var result = writer.ToString();
+
+                Assert.IsTrue(result.Contains(entity.Parent.Name), "Parent.Name");
+                Assert.IsTrue(result.Contains(entity.Parent.Version), "Parent.Version");
+                Assert.IsTrue(result.Contains(entity.Child.Name), "Child.Name");
+                Assert.IsTrue(result.Contains(entity.Child.Version), "Child.Version");
+            }
+        }
+    }
+}
