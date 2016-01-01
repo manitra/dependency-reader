bae81cf41498fe3065ba3ff4f8fd7c1654b29bbb
Manitra Andriamitondra <m.andriamitondra@criteo.com>
2016-01-02 00:56:54 +0700

    Using nuget 2.X

--- build/build.sh
+++ build/build.sh
@@ -4,6 +4,6 @@ REPO=$(readlink -f $(dirname $0)/..)
 
 # Building c# and front
 cd $REPO/src
-wget -O nuget.exe http://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+wget -O nuget.exe https://dist.nuget.org/win-x86-commandline/v2.8.6/nuget.exe
 mono nuget.exe restore DependencyReader.sln -NoCache -Source https://www.nuget.org/api/v2/
 xbuild DependencyReader.sln /p:Configuration=Release
