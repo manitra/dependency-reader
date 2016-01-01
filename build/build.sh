9cc02a54a4e3aa2f4c14341574e417dd6fc02e36
Manitra Andriamitondra <m.andriamitondra@criteo.com>
2016-01-02 00:34:30 +0700

    Specify src

--- build/build.sh
+++ build/build.sh
@@ -5,5 +5,5 @@ REPO=$(readlink -f $(dirname $0)/..)
 # Building c# and front
 cd $REPO/src
 wget -O nuget.exe http://dist.nuget.org/win-x86-commandline/latest/nuget.exe
-mono nuget.exe restore DependencyReader.sln -NoCache
+mono nuget.exe restore DependencyReader.sln -NoCache -Source https://www.nuget.org/api/v2/
 xbuild DependencyReader.sln /p:Configuration=Release
