bc48ba81851d54b8e4309568c058352a58db7056
Manitra Andriamitondra <m.andriamitondra@criteo.com>
2016-01-07 10:27:58 +0700

    Fixed test result location

--- build/test.sh
+++ build/test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd $(readlink -f $(dirname $0)/..)
-mono src/packages/NUnit.Runners.2.6.3/tools/nunit-console.exe src/DependencyReader.CLI.Tests/bin/Release/DependencyReader.CLI.Tests.dll --output=bin/TestResult.xml
+mono src/packages/NUnit.Runners.2.6.3/tools/nunit-console.exe src/DependencyReader.CLI.Tests/bin/Release/DependencyReader.CLI.Tests.dll --work bin
